e2b194639b5cd1688baf924870372e20fc15d37f
Pedro Taborda <ptaborda@ebay.com>
2016-06-09 10:07:12 +0200

    post a url within a json object, respond it back in text

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser')
+
 var app = express();
+
+app.use(bodyParser.json())
+
+// post a url
+app.post('/url', function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+  res.send('body: ' + req.body.url)
+})
+
+// post an actual image
+app.post('/img', function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+  res.send('welcome: ' + req.body.username)
+})
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Clarifai client running on port 3000!');
 });
